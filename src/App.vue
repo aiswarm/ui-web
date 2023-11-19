fc34467559e7c081fd7a28d6880365b126a7c9b4
Ravi Gairola <mallox@pyxzl.net>
2023-11-18 21:13:56 -0800

    Prettier stub interface

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template lang="pug">
-div
-  Sidebar
-  div
-    ChatLog
+v-app
+  v-navigation-drawer(v-model="drawer" app)
+    Sidebar
+  v-main
+    v-container(fluid)
+      v-row
+        v-col(cols="12")
+          v-row(height="100%" justify="center" align="center")
+            ChatLog
+  v-footer(app).fixed.bottom
     ChatBar
 </template>
 
 <script>
-import Sidebar from "./components/SideBar.vue";
-import ChatLog from "./components/ChatLog.vue";
-import ChatBar from "./components/ChatBar.vue";
+import Sidebar from './components/SideBar.vue'
+import ChatLog from './components/ChatLog.vue'
+import ChatBar from './components/ChatBar.vue'
 
 export default {
   components: {
@@ -17,7 +23,13 @@ export default {
     ChatLog,
     ChatBar,
   },
-};
+  data: () => ({
+    drawer: null,
+  }),
+}
 </script>
 
-<style lang="stylus"></style>
+<style lang="stylus">
+.v-main__wrap
+  overflow auto
+</style>
