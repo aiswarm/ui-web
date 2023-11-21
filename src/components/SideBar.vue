bda6fe5160ea9300becd3480de5e489ee6e77ee4
Ravi Gairola <mallox@pyxzl.net>
2023-11-21 15:22:01 -0800

    Connected Sidebar & ChatLog

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,40 +1,40 @@
 <template lang="pug">
 div
-    h2 Groups
-    ul(v-if="groups.length")
-        li(v-for="group in groups" :key="group.name") {{ group.name }}
+  h2 Groups
+  ul(v-if="groups.length")
+    li(v-for="group in groups" :key="group.name" @click="selectChat(group.name, 'group')") {{ group.name }}
     v-btn(icon="mdi-plus" @click="showAddGroupDialog = true" density="compact" )
     v-dialog(v-model="showAddGroupDialog", max-width="290")
-        v-card
-            v-card-title
-                span.headline Add Group
-            v-card-text
-                v-text-field(v-model="newGroupName" label="Group Name" @keydown.enter="addGroup" @keydown.esc="showAddGroupDialog = false")
-            v-card-actions
-                v-spacer
-                v-btn(text @click="showAddGroupDialog = false") Cancel
-                v-btn(color="blue darken-1", text @click="addGroup") Add
+      v-card
+        v-card-title
+          span.headline Add Group
+        v-card-text
+          v-text-field(v-model="newGroupName" label="Group Name" @keydown.enter="addGroup" @keydown.esc="showAddGroupDialog = false")
+        v-card-actions
+          v-spacer
+          v-btn(text @click="showAddGroupDialog = false") Cancel
+          v-btn(color="blue darken-1", text @click="addGroup") Add
     h2 Agents
     ul(v-if="agents.length")
-        li(v-for="agent in agents" :key="agent.name") {{ agent.name }}
+      li(v-for="agent in agents" :key="agent.name" @click="selectChat(agent.name, 'agent')") {{ agent.name }}
     v-btn(icon="mdi-plus" @click="showAddAgentDialog = true" density="compact" )
     v-dialog(v-model="showAddAgentDialog" max-width="290")
-        v-card
-            v-card-title
-                span.headline Add Agent
-            v-card-text
-                v-text-field(v-model="newAgentName" label="Agent Name" @keydown.enter="addAgent" @keydown.esc="showAddAgentDialog = false")
-                v-select(v-model="selectedDriver" :items="drivers.map(driver => driver.type)" label="Driver" @keydown.enter="addAgent" @keydown.esc="showAddAgentDialog = false")
-            v-card-actions
-                v-spacer
-                v-btn(text @click="showAddAgentDialog = false") Cancel
-                v-btn(color="blue darken-1" text @click="addAgent") Add
+      v-card
+        v-card-title
+          span.headline Add Agent
+        v-card-text
+          v-text-field(v-model="newAgentName" label="Agent Name" @keydown.enter="addAgent" @keydown.esc="showAddAgentDialog = false")
+          v-select(v-model="selectedDriver" :items="drivers.map(driver => driver.type)" label="Driver" @keydown.enter="addAgent" @keydown.esc="showAddAgentDialog = false")
+        v-card-actions
+          v-spacer
+          v-btn(text @click="showAddAgentDialog = false") Cancel
+          v-btn(color="blue darken-1" text @click="addAgent") Add
 </template>
 
 <script setup>
 import { useMutation, useQuery, useSubscription } from '@vue/apollo-composable'
 import { gql } from '@apollo/client/core'
-import { ref, watchEffect } from 'vue'
+import { defineEmits, ref, watchEffect } from 'vue'
 
 const groups = ref([])
 const agents = ref([])
@@ -103,6 +103,12 @@ watchEffect(() => {
     }
   })
 })
+
+const emit = defineEmits(['chat-selected'])
+
+const selectChat = (name, type) => {
+  emit('chat-selected', { name, type })
+}
 
 // Groups
 
