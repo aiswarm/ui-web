2d105b1db2c7dc01c114cc0df2e393edd0756c12
Ravi Gairola <mallox@pyxzl.net>
2023-12-04 16:57:05 -0800

    Clean up & first implementation running with OpenAI

--- src/components/GroupList.vue
+++ src/components/GroupList.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
 h3 Groups
 v-list(v-if="groups.length")
-    v-list-item(v-for="group in groups" :key="group.name" @click="selectGroup(group.name)" :class="{ 'selected-chat': group.name === targetSelected.name, badge: true }")
-        v-badge(v-if="group.count > 0" :content="group.count" floating=true offset-y="5" offset-x="-5" label="{{group.name}}" color="#f0f0f0" ) {{ group.name }}
-        span(v-else) {{ group.name }}
-    v-list-item
-        v-btn(icon="mdi-plus" @click="showAddGroupDialog = true" density="compact")
-        v-dialog(v-model="showAddGroupDialog", max-width="290")
-            v-card
-                v-card-title
-                    span.headline Add Group
-                v-card-text
-                    v-text-field(v-model="newGroupName" label="Group Name" autofocus=true)
-                v-card-actions
-                    v-spacer
-                    v-btn(text @click="showAddGroupDialog = false") Cancel
-                    v-btn(color="blue darken-1" text @click="addGroup") Add
+  v-list-item(v-for="group in groups" :key="group.name" @click="selectGroup(group.name)" :class="{ 'selected-chat': group.name === targetSelected.name, badge: true }")
+    v-badge(v-if="group.count > 0" :content="group.count" floating=true offset-y="5" offset-x="-5" label="{{group.name}}" color="#f0f0f0" ) {{ group.name }}
+    span(v-else) {{ group.name }}
+  v-list-item
+    v-btn(icon="mdi-plus" @click="showAddGroupDialog = true" density="compact")
+    v-dialog(v-model="showAddGroupDialog", max-width="290")
+      v-card
+        v-card-title
+          span.headline Add Group
+        v-card-text
+          v-text-field(v-model="newGroupName" label="Group Name" autofocus=true)
+        v-card-actions
+          v-spacer
+          v-btn(text @click="showAddGroupDialog = false") Cancel
+          v-btn(color="blue darken-1" text @click="addGroup") Add
 </template>
 
 <script setup>
@@ -28,30 +28,30 @@ const groups = inject('groups')
 const message = inject('message')
 
 watch(message,
-    (message) => {
-        const group = groups.value.find((group) => group.name === message.target)
-        if (!group) {
-            return
-        }
-        if (targetSelected.value.name !== message.target) {
-            group.count++
-        }
-    })
+  (message) => {
+    const group = groups.value.find((group) => group.name === message.target)
+    if (!group) {
+      return
+    }
+    if (targetSelected.value.name !== message.target) {
+      group.count++
+    }
+  })
 
 /**
  *  Select a group and resets the count of unread messages.
- * @param {String} name
+ * @param {string} name
  */
 const selectGroup = (name) => {
-    targetSelected.value = {name, type: 'group'}
-    const group = groups.value.find((group) => group.name === name)
-    if (group) {
-        group.count = 0
-    }
+  targetSelected.value = {name, type: 'group'}
+  const group = groups.value.find((group) => group.name === name)
+  if (group) {
+    group.count = 0
+  }
 }
 
 const {mutate: addGroupMutation} = useMutation(
-    gql`
+  gql`
     mutation CreateGroup($name: String!) {
       createGroup(name: $name)
     }
@@ -62,11 +62,11 @@ const showAddGroupDialog = ref(false)
 const newGroupName = ref('')
 
 async function addGroup() {
-    if (newGroupName.value.trim() !== '') {
-        await addGroupMutation({name: newGroupName.value})
-        showAddGroupDialog.value = false
-        newGroupName.value = ''
-    }
+  if (newGroupName.value.trim() !== '') {
+    await addGroupMutation({name: newGroupName.value})
+    showAddGroupDialog.value = false
+    newGroupName.value = ''
+  }
 }
 </script>
 
