4dc1bfca9afa928c9ca7a3919d2699a042ed7c88
Ravi Gairola <mallox@pyxzl.net>
2024-03-19 19:52:45 -0700

    Support global npm installation

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import sirv from 'sirv'
 import cors from 'cors'
 import {promisify} from 'util'
 import fs from 'fs'
+import {execSync} from 'child_process'
 
 export async function startServer(log, port = 3000) {
   const app = polka()
@@ -17,19 +18,34 @@ export async function startServer(log, port = 3000) {
   app.use(cors())
 
   // Serve static files from the specified directory
-  const staticDir = path.join(process.cwd(), 'node_modules/@aiswarm/ui-web/dist')
-  if (!fs.existsSync(staticDir)) {
-    throw new Error(
-      'Could not find UI files, please run \'npm run build\' in the ui-web directory first.'
-    )
+  const staticLocalDir = path.join(process.cwd(), 'node_modules/@aiswarm/ui-web/dist')
+  if (fs.existsSync(staticLocalDir)) {
+    app.use(sirv(staticLocalDir))
+  } else {
+    log.trace('Could not find local UI files, looking in global node_modules directory.')
+    const staticGlobalDir = path.join(getGlobalNodeModulesPath(log), '@aiswarm/ui-web/dist')
+    if (fs.existsSync(staticGlobalDir)) {
+      app.use(sirv(staticGlobalDir))
+    } else {
+      throw new Error(
+        'Could not find UI files, please run \'npm run build\' in the ui-web directory first.'
+      )
+    }
   }
-  app.use(sirv(staticDir))
 
   // Start the server
   const listen = promisify(app.listen).bind(app)
   await listen(port)
 
   log.info(`Web UI is available at http://localhost:${port}`)
+}
+
+function getGlobalNodeModulesPath(log) {
+  try {
+    return execSync('npm root -g').toString().trim()
+  } catch (error) {
+    log.debug('Error getting global node_modules directory:', error)
+  }
 }
 
 /**
